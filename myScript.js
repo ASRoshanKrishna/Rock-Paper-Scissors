fb51c059350eb7ef37ba5d39e118d55e98dbf7f3
Roshan Krishna A.S <asroshank@gmail.com>
2023-12-23 20:56:54 +0530

    add getComputerChoice

--- myScript.js
+++ myScript.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+function getComputerChoice(){
+    let c = Math.floor(Math.random() * 3);
+    switch(c){
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
+
+let n = getComputerChoice();
+
+console.log(n);
